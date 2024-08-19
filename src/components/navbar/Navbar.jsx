f5a5b93f02912141c8e04e9a21806ea8948dd382
shadik <shadikmahamudsohan@gmail.com>
2024-08-19 22:06:20 +0600

    navbar and sidebar added

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar />
+      <div className="wrapper">
+        <span>Shadik Mahamud</span>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribbble.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
